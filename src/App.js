78d7fdd7eeca34974ab30e3df767311cfafd6f61
Nick Playfair <nick@chaosemerald.co.uk>
2018-08-20 20:23:26 +0100

    form to add entries

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, {Component} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
 
   state = {
-    characters: [
-      {
-        'name': 'Charlie',
-        'job': 'Janitor'
-      },
-      {
-        'name': 'Mac',
-        'job': 'Bouncer'
-      },
-      {
-        'name': 'Dee',
-        'job': 'Aspiring actress'
-      },
-      {
-        'name': 'Dennis',
-        'job': 'Bartender'
-      }
-    ]
+    characters: []
   };
 
   removeCharacter = index => {
@@ -34,6 +18,10 @@ class App extends Component {
     });
   }
 
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+  }
+
   render() {
     const { characters } = this.state;
 
@@ -43,6 +31,7 @@ class App extends Component {
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
